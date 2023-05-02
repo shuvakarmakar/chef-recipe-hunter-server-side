41991e194a39765996b0e82fcda256ef1c8b46e1
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-03 00:55:21 +0600

    recipes json file loaded

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require('cors');
 const port = process.removeListener.PORT || 5000;
 
+// Require Data From Json File
 const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
 
 app.use(cors());
 
@@ -14,6 +16,12 @@ app.get('/', (req, res) =>{
 app.get('/chefs', (req, res) =>{
     res.send(chefs);
 })
+
+app.get('/recipes', (req, res) =>{
+    res.send(recipes);
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`Chefs Website is Running on port: ${port}`);
